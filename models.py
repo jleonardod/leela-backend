c161822a918fdf0a453aa48cfdea2b6d5e0432ba
Leonardo Diaz <jldiaz9623@gmail.com>
2025-06-11 16:10:22 -0500

    Modelo de tablas

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from datetime import datetime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+    hashed_password = Column(String(200))
+    name = Column(String(100))
+    is_active = Column(Boolean, default=True)
+
+class Project(Base):
+    __tablename__ = "projects"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))
+    owner_id = Column(Integer, ForeignKey=("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))
+    description = Column(String(100))
+    status = Column(String(50), default="To Do")
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
